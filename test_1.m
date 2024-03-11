e9706e5d48d59b47e13f6a004ce54048f397efb5
Jose Rugeles Uribe <jose.rugeles@unimilitar.edu.co>
2024-03-10 20:48:43 -0500

    Add files via upload

---
+++ test_1.m
@@ -0,0 +1,11 @@
+% Programa de ejemplo para trabajo en equipo mediante github classroom
+% Los estudiantes deben crear un código de forma colectiva donde participen
+% TODOS para obtener una señal en el tiempo creada a partir de la suma de
+% sus armónicos. El número de armonicos corresponde a la cantidad de
+% estudiantes del curso.La amplitud de la señal es 10 Vp y la frecuencia
+% fundamental es  3KHz. 
+% Los estudiantes deben crear el código de forma colectiva y obtener una
+% figuras con dos subfiguras organizadas de forma horizontal. La primera
+% contiene la señal en el tiempo creada a partir de los armonicos y la
+% segunda contiene todos los armonicos en el tiempo superpuestos y
+% diferenciados por colores.
